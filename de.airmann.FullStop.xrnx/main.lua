3026080a2becb9b17f453731895e1dc83daa7ddd
airmann_pub <airmann_pub@online.de>
2012-01-31 01:23:06 +0000

    mod

--- de.airmann.FullStop.xrnx/main.lua
+++ de.airmann.FullStop.xrnx/main.lua
@@ -2,9 +2,10 @@
 
   Full Stop
   
-  provides a simple new play/stop command "play/panic stop = full sound stop". 
-  This is an alternative to the Renoise default "play/stop" command, which doesn't
-  stop DSP output like reverb/delay tails.
+  provides a simple new play/stop command "play/panic stop = full sound stop",
+  Technically simply combines "play / panic stop" into a new command.
+  The command can be used as an alternative to the Renoise default "play/stop"
+  command, which doesn't stop DSP output like reverb/delay tails.
   
   This command can be assigned to any key (e.g. space) or midi controller. 
   
@@ -47,6 +48,54 @@ function play_full_stop_hard()
     renoise.song().transport:start(start_mode)  
   end
 end
+
+function play_full_stop_ramped()
+  
+  if (renoise.song().transport.playing) then
+  
+    -- Add a new timer for ramping down/up
+    if (not renoise.tool():has_timer(ramp_down())) then
+      renoise.tool():add_timer(ramp_down(),500)
+    end
+  
+    --local tracks = renoise.song().tracks
+    --for t = 1,#tracks do
+    --  if (tracks[t].type == renoise.Track.TRACK_TYPE_MASTER) then
+    --    renoise.song().tracks[t].postfx_volume.value = 0.0
+    --  end
+    --end
+
+    --renoise.song().transport:panic()
+  else
+
+    local tracks = renoise.song().tracks
+    for t = 1,#tracks do
+      if (tracks[t].type == renoise.Track.TRACK_TYPE_MASTER) then
+        renoise.song().tracks[t].postfx_volume.value = math.db2lin(0)
+      end
+    end
+  
+    local start_mode = renoise.Transport.PLAYMODE_RESTART_PATTERN
+    renoise.song().transport:start(start_mode)  
+  end
+end
+
+function ramp_down()
+  --for t = 1,#renoise.song().tracks do
+  print("start")
+  --  if (renoise.song().tracks[t].type == renoise.Track.TRACK_TYPE_MASTER) then
+  --    local current_value = renoise.song().tracks[t].postfx_volume.value - math.db2lin(10)           
+  --    if (current_value < 0.0) then
+  --      current_value = 0.0
+        renoise.tool():remove_timer(ramp_down())
+  --    end
+  --    print(current_value) 
+  --    renoise.song().tracks[t].postfx_volume.value = current_value
+  --    return;
+  --  end
+  --end      
+end
+
 
 --[[ debug ]]--------------------------------------------------------------]]--
 
