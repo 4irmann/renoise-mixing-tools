38cd58968a0987a198ee6e673ad1809e9973b6d1
airmann_pub <airmann_pub@online.de>
2012-02-03 02:42:26 +0000

    mod

--- de.airmann.FoobarRemoteControl.xrnx/main.lua
+++ de.airmann.FoobarRemoteControl.xrnx/main.lua
@@ -39,9 +39,11 @@
   License for the specific language governing permissions and limitations 
   under the License. 
   
+ 
 ----------------------------------------------------------------------------]]--
 
 require "Helpers"
+
 
 -- default preferences
 local prefs = renoise.Document.create("FoobarRemoteControlPreferences") { 
@@ -54,12 +56,18 @@ local prefs = renoise.Document.create("FoobarRemoteControlPreferences") {
     connection_timeout = 2000,
     receive_timeout = 500,
     
-    -- Xfade Type
-    -- 0 = Constant Power (n=0)
-    -- 1 = Constant Power Slow Fade (n=1)
-    -- 3 = Constant Power Slow Cut (n=3)
-    -- 10 = Constant Power Fast Cut (n=10)
-    xfade_type = 0
+    -- Xfade default Type, either:
+    -- dipped
+    -- intermediate
+    -- const_power
+    -- slow_fade
+    -- slow_cut
+    -- fast_cut
+    -- transition
+    xfade_default_type = "fast_cut",
+    
+    -- link foobar volume and xfade volume
+    linked_volumes = true
 }
 
 -- data structure for virtual device parameters
@@ -72,35 +80,37 @@ function VirtualDeviceParameter:__init()
   
   self.name = "n/a"
   self.polarity = renoise.DeviceParameter.POLARITY_UNIPOLAR
-  self.value_min = 0
-  self.value_max = 127
+  self.value_min = 0.0
+  self.value_max = 1.0
   self.value_quantum = 0.5
-  self.value_default = 127
-  self.value = 0    
+  self.value_default = 1.0
+  self.value = 0.0
 end
 
--- stores foobar volume (0..100)
+-- stores foobar volume 
 local fb2k_volume = VirtualDeviceParameter()
 fb2k_volume.name = "fb2k_foobar_volume"
-fb2k_volume.value_min = 0
-fb2k_volume.value_max = 100
-fb2k_volume.value = 100
+fb2k_volume.value_default = 1.0
+fb2k_volume.value = 1.0
 
--- stores foobar playing position (0..100)
+-- stores foobar xfade volume
+local fb2k_xfade_volume = VirtualDeviceParameter()
+fb2k_xfade_volume.name = "fb2k_foobar_xfade_volume"
+fb2k_xfade_volume.value_default = 0.0
+fb2k_xfade_volume.value = 0.0
+
+-- stores foobar playing position
 local fb2k_position = VirtualDeviceParameter()
 fb2k_position.name = "fb2k_foobar_position"
-fb2k_position.value_min = 0
-fb2k_position.value_max = 100
-fb2k_position.value = 0
 
--- stores xfade position (7bit 0..127)
+-- stores xfade position
 -- 0 = only Renoise
--- 127 = only foobar
+-- 1.0 = only foobar
 local fb2k_xfade_position = VirtualDeviceParameter() 
 fb2k_xfade_position.name = "fb2k_xfade_pos"
-fb2k_xfade_position.value = 127
-              
-
+fb2k_xfade_position.value_default = 1.0
+fb2k_xfade_position.value = 1.0
+           
 if (io.exists("config.xml")) then
   prefs:load_from("config.xml")
 else
@@ -113,7 +123,7 @@ renoise.tool():add_keybinding {
   invoke = function() fb2k_start() end
 }
 renoise.tool():add_midi_mapping {
-  name = "Global:Tools:FB2k Start [Trigger]",
+  name = "Global:Tools:FB2K Start [Trigger]",
   invoke =  function(message) 
               if (message:is_trigger()) then
                 fb2k_start() 
@@ -127,7 +137,7 @@ renoise.tool():add_keybinding {
   invoke = function() fb2k_stop() end 
 }
 renoise.tool():add_midi_mapping {
-  name = "Global:Tools:FB2k Stop",
+  name = "Global:Tools:FB2K Stop",
   invoke =  function(message)
               if (message:is_trigger()) then
                 fb2k_stop() 
@@ -135,14 +145,13 @@ renoise.tool():add_midi_mapping {
             end  
 }
 
-
 -- Play/Pause
 renoise.tool():add_keybinding {
   name = "Global:Tools:FB2K PlayOrPause",
   invoke = function() fb2k_play_or_pause() end
 }
 renoise.tool():add_midi_mapping {
-  name = "Global:Tools:FB2k PlayOrPause [Trigger]",
+  name = "Global:Tools:FB2K PlayOrPause [Trigger]",
   invoke =  function(message) 
               if (message:is_trigger()) then
                 fb2k_play_or_pause() 
@@ -156,7 +165,7 @@ renoise.tool():add_keybinding {
   invoke = function() fb2k_start_next() end
 }
 renoise.tool():add_midi_mapping {
-  name = "Global:Tools:FB2k StartNext [Trigger]",
+  name = "Global:Tools:FB2K StartNext [Trigger]",
   invoke =  function(message) 
               if (message:is_trigger()) then
                 fb2k_start_next() 
@@ -170,7 +179,7 @@ renoise.tool():add_keybinding {
   invoke = function() fb2k_start_previous() end
 }
 renoise.tool():add_midi_mapping {
-  name = "Global:Tools:FB2k StartPrevious [Trigger]",
+  name = "Global:Tools:FB2K StartPrevious [Trigger]",
   invoke =  function(message) 
               if (message:is_trigger()) then
                 fb2k_start_previous() 
@@ -180,16 +189,20 @@ renoise.tool():add_midi_mapping {
 
 -- Volume (so far just midi)
 renoise.tool():add_midi_mapping {
-  name = "Global:Tools:FB2k SetVolume [Set]",
+  name = "Global:Tools:FB2K SetVolume [Set]",
   invoke =  function(message)               
               fb2k_volume.value = parameter_message_value(message,fb2k_volume)              
-              fb2k_set_volume(fb2k_volume)
+              if (prefs.linked_volumes.value == true) then
+                fb2k_set_volume_db(fb2k_linked_volume_value())
+              else
+                fb2k_set_volume_db(fb2k_volume.value)
+              end                
             end            
 }
 
 -- Seek (so far just midi)
 renoise.tool():add_midi_mapping {
-  name = "Global:Tools:FB2k SeekTo [Set]",
+  name = "Global:Tools:FB2K SeekTo [Set]",
   invoke =  function(message)               
               fb2k_position.value = parameter_message_value(message,fb2k_position)                           
               fb2k_seek_to(fb2k_position) 
@@ -198,40 +211,95 @@ renoise.tool():add_midi_mapping {
 
 --- XFade Renoise-Foobar (so far just midi)
 renoise.tool():add_midi_mapping {
-  name = "Global:Tools:FB2k XFadeRenoiseFB2K [Set]",
+  name = "Global:Tools:FB2K XFadeRenoiseFB2K [Set]",
   invoke =  function(message)                 
               fb2k_xfade_position.value = parameter_message_value(message,fb2k_xfade_position)              
               fb2k_xfade(fb2k_xfade_position) 
             end            
 }
 
--- precision: nr of decimal places
+-- simple rounding function, precision: nr of decimal places
+-- return int value
 function round(value,decimalPlaces)
   return math.floor(value*10^decimalPlaces+0.5)/10^decimalPlaces 
 end
 
---  Constant Power XFade
+function fb2k_xfade_dipped(x)
+  return 1.0-x^2
+end
+
+function fb2k_xfade_intermediate(x)
+  return 1.0-x
+end
+
+-- constant power xfade
+-- provides constant power (n=0), slow fade (n=1), slow cut (n=3), fast cut (n=10)
 -- See http://math.stackexchange.com/questions/4621/simple-formula-for-curve-of-dj-crossfader-volume-dipped
--- x: 0.0 - 1.0 
--- y: 0.0 - 1.0
-function fb2k_const_pow_xfade(x,n)   
+-- x: 0.0 .. 1.0 
+-- y: 0.0 .. 1.0
+function fb2k_xfade_const_power(x,n)   
   return math.cos(math.pi*0.25*(((2*x-1)^(2*n+1)+1)))
 end
 
--- XFade
-function fb2k_xfade(devPara)
+function fb2k_xfade_transition(x) 
+    local y =2.0*(1.0-x)
+    if y >= 1.0 then y = 1.0 end
+    return y
+end
 
-  print("XFade Pos: "..devPara.value)
-  local x = devPara.value / devPara.value_max
-  local n = prefs.xfade_type.value
+-- XFade
+function fb2k_xfade(devPara,xfade_type)
+
+  print("XFade Pos: "..(devPara.value*100))
+  local x = devPara.value
+  local n = 0 
+  local xfade_func
   
-  local renoise_vol = fb2k_const_pow_xfade(x,n) 
-  master_track().postfx_volume.value = renoise_vol
-  print("REN VOL: "..renoise_vol)
+  if (xfade_type == nil) then
+    xfade_type = prefs.xfade_default_type.value
+  end  
+  
+  if (xfade_type == "dipped") then
+    xfade_func = fb2k_xfade_dipped  
+  
+  elseif (xfade_type == "intermediate") then
+    xfade_func = fb2k_xfade_intermediate
     
-  fb2k_volume.value = 
-    fb2k_const_pow_xfade(1.0-x,n)*fb2k_volume.value_max
-  fb2k_set_volume(fb2k_volume)  
+  elseif (xfade_type == "const_power") then
+    n = 0
+    xfade_func = fb2k_xfade_const_power
+  
+  elseif (xfade_type == "slow_fade") then
+    n = 1
+    xfade_func = fb2k_xfade_const_power  
+    
+  elseif (xfade_type == "slow_cut") then
+    n = 3
+    xfade_func = fb2k_xfade_const_power  
+  
+  elseif (xfade_type == "fast_cut") then
+    n = 10
+    xfade_func = fb2k_xfade_const_power  
+  
+  elseif (xfade_type == "transition") then
+    xfade_func = fb2k_xfade_transition
+  
+  end
+      
+  local renoise_vol = xfade_func(x,n)
+  print("REN VOL: "..renoise_vol)  
+  master_track().postfx_volume.value = renoise_vol
+  
+   
+  fb2k_xfade_volume.value = xfade_func(1.0-x,n)   
+  local foobar_vol
+  if (prefs.linked_volumes.value == true) then
+    foobar_vol = fb2k_linked_volume_value()    
+  else
+    foobar_vol = fb2k_xfade_volume.value
+  end    
+  print("FOO VOL: "..foobar_vol)
+  fb2k_set_volume_db(foobar_vol)
 end
 
 -- Start
@@ -259,17 +327,38 @@ function fb2k_start_previous(param1)
   fb2k_cmd("StartPrevious", param1)
 end
 
--- Volume 
-function fb2k_set_volume(devPara)  
-  local value = math.floor(devPara.value+devPara.value_quantum)
-  fb2k_cmd("Volume",value)
-  print("FOO VOL: "..value)
+-- Linked foobar / xfade volume
+function fb2k_linked_volume_value()
+  return fb2k_volume.value * fb2k_xfade_volume.value
 end
 
--- Seek 
-function fb2k_seek_to(devPara)
-  local value = math.floor(devPara.value+devPara.value_quantum)
+-- actually we don't need it, since set_volume_db is more precise
+-- Volume : linear (0.0..1.0 float) f1()->  linear (0..100 uint) f(2)-> log  (-100..0 dB float)
+--function fb2k_set_volume(devPara)   
+--  local value = math.floor(devPara.value*100+0.5)
+--  fb2k_cmd("Volume",value)
+--  print("FOO VOL: "..value)
+--end   
+
+-- VolumeDB : linear (0.0..1.0 float) f1()-> log (1000..0 uint) f(2)-> log (-100..0 dB float)
+function fb2k_set_volume_db(value)   
+  local foobar_value = math.floor(math.lin2db(value)*-10+0.5)  
+  
+  -- Renoise volume range <= 0 dB: from -200 dB .. +0 dB -> 10^-10 .. 1.0
+  -- foobar  volume range <= 0 dB: from -100 dB .. +0 dB -> 10^-5 .. 1.0 -> 1000 .. 0
+  if (foobar_value > 1000) then 
+    foobar_value = 1000
+  end  
+  
+  print("FOO VOL DB: "..foobar_value)
+  fb2k_cmd("VolumeDB",foobar_value)  
+end   
+
+-- Seek : linear (0.0..1.0 float) f1() -> (0..100 uint)
+function fb2k_seek_to(devPara)  
+  local value = math.floor(devPara.value*100+0.5)
   fb2k_cmd("Seek",value)
+  print("FOO SEEK:"..value)
 end
   
 -- HTTP / GET client
@@ -281,7 +370,9 @@ function fb2k_cmd(cmd,param1)
   local http_server = prefs.foobar_http_control_server.value
   local http_server_port = prefs.foobar_http_control_server_port.value
     
-  local client, socket_error = renoise.Socket.create_client(http_server, http_server_port,renoise.Socket.PROTOCOL_TCP, connection_timeout)
+  local client, socket_error = 
+    renoise.Socket.create_client(http_server, 
+      http_server_port,renoise.Socket.PROTOCOL_TCP, connection_timeout)
    
   if socket_error then 
     renoise.app():show_warning(socket_error)
